ba5aeccf3550b8243f81324c7b852b57482de52a
Vastatio <solarcactus@live.com>
2016-08-06 13:22:08 -0700

    Just added 4 methods to MathOperation.java

--- src/com/math/helpers/MathOperation.java
+++ src/com/math/helpers/MathOperation.java
@@ -2,4 +2,9 @@ package com.math.helpers;
 
 public interface MathOperation {
 
+	public Object add(Object a, Object b);
+	public Object subtract(Object a, Object b);
+	public Object multiply(Object a, Object b);
+	public Object divide(Object a, Object b);
+	
 }
